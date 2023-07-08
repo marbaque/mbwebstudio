7c1c2eb0a5a303b87ff90120bf07fb8afcbc97d4
mario <mario.badilla@gmail.com>
2023-07-08 15:51:19 -0600

    favicon y gsap nubes

---
+++ js/app.js
@@ -0,0 +1,30 @@
+gsap.to(".nube1", {
+  duration: 5,
+  left: "-10%",
+  ease: "sine.in",
+});
+
+gsap.to(".nube2", {
+  duration: 5,
+  left: "-5%",
+  ease: "sine.in",
+});
+gsap.to(".nube3", {
+  duration: 5,
+  right: "-10%",
+  ease: "sine.in",
+});
+
+gsap.to(".title", {
+    duration: 3,
+    delay: 1,
+    opacity: 1,
+    ease: "sine.in",
+});
+gsap.to(".subtitle", {
+    duration: 4,
+    delay: 1,
+    opacity: 1,
+    zIndex: 99,
+    ease: "sine.in",
+});
